ab89857cf354e4d2429f4c2bcc49c5566ae29c49
akjhaa007 <akjhaa007@gmail.com>
2017-05-25 00:59:20 +0530

    Add files via upload

---
+++ SpEED_CONTROL_WITH_Value.ino
@@ -0,0 +1,21 @@
+int potPin = A0;
+int motorPin = 9;
+int potValue = 0;
+int motorValue = 0;
+void setup()
+{
+ Serial.begin(9600);
+}
+void loop() {
+ potValue = analogRead(potPin);  
+ motorValue = map(potValue, 0, 1000, 0, 255);
+ 
+ //digitalWrite(motorPin,motorValue);
+ 
+ analogWrite(motorPin, motorValue);  
+ Serial.print("potentiometer = " );     
+ Serial.print(potValue);
+ Serial.print("t motor = ");
+ Serial.println(motorValue);
+ delay(2);    
+}
